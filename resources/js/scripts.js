2f655e330b0fcb0ff95ea62826f1f67dc37492cf
suraj_sloth <suraj.mahto49@gmail.com>
2019-10-23 17:23:59 +0530

    hackatron image upgrade

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,32 +1,16 @@
 
-/*
-$(document).ready(function() {
-	$(window).scroll(function() {
-
-		if($(window).scrollTop() < 299) {
-			$('#hero-nav').removeClass('fixed-top');
-		}
-
-		if($(window).scrollTop() > 300) {
-			$('#hero-nav').addClass('fixed-top');
-		}
-
-	}
-	);
-
-*/
-
 $(document).ready(function() {
   $(window).scroll(function () { 
 
     console.log($(window).scrollTop());
 
-    if ($(window).scrollTop() > 550) {
+    if ($(window).scrollTop() < 550) {
+      $('#hero-nav').removeClass('fixed-top');
+    }
+
+    if ($(window).scrollTop() > 551) {
       $('#hero-nav').addClass('fixed-top');
     }
 
-    if ($(window).scrollTop() < 551) {
-      $('#hero-nav').removeClass('fixed-top');
-    }
   });
 });
