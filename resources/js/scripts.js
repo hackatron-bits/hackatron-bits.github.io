4513a99f270569ccff689fd001890348a382f2bf
snehal2398 <snehal2398@gmail.com>
2019-11-01 14:46:43 +0530

    preloader added

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -14,3 +14,9 @@ $(document).ready(function() {
 
   });
 });
+
+var overlay = document.getElementById("overlay");
+
+window.addEventListener('load', function(){
+  overlay.style.display = 'none';
+})
